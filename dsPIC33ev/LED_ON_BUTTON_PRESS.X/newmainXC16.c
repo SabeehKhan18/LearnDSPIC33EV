268d830ed0cbca9b315767f5a54ca9db4c932dac
George Main IV <george.main@gemiv.net>
2017-06-03 10:33:24 -0400

    Created LED ON BUTTON PRESS project

---
+++ dsPIC33ev/LED_ON_BUTTON_PRESS.X/newmainXC16.c
@@ -0,0 +1,15 @@
+/*
+ * File:   newmainXC16.c
+ * Author: George Main IV
+ *
+ * This project will turn on an LED when a button is pressed
+ * 
+ * Created on June 3, 2017, 10:32 AM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
