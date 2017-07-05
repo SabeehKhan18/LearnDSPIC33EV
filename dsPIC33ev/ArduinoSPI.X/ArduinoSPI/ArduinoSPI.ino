1db1a6bc3ae29932f7dcf82362651ceaaa7b6891
George Main IV <george.main@gemiv.net>
2017-07-05 16:54:16 -0400

    created

---
+++ dsPIC33ev/ArduinoSPI.X/ArduinoSPI/ArduinoSPI.ino
@@ -0,0 +1,17 @@
+/*
+ * File:   SPI.c
+ * Author: George Main IV
+ * 
+ * This project sends and receives data to and from an Arduino using SPI
+ *
+ * Created on July 5, 2017, 4:53 PM
+ */
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
